e1d1d63d0642dfc39e1208396b3cc86cfc7053cb
Tessa Erika <116341897+AssetAkire@users.noreply.github.com>
2024-02-20 02:15:50 +0800

    check_palindrome.py

--- day-05/TASKS/check_palindrome.py
+++ day-05/TASKS/check_palindrome.py
@@ -1,4 +1,7 @@
 # Write the code ↓ to prompt the user to enter a word.
+print("PALINDROME CHECKER FOR ALF")
+
+word = input("Please, enter a word/s to check: ")
 # Be cautious when reading input of various data types.
 
 
@@ -8,6 +11,7 @@
 
 
 # Write the code ↓ to check if the entered word is a palindrome.
+reversed_word = word[::-1]
 # Utilize string functions to compare the original word with its reverse.
 
 
@@ -16,6 +20,10 @@
 
 
 # Write the code ↓ to display whether the entered word is a palindrome or not.
+if reversed_word == word:
+    print("'%s' is a palindrome" %(word))
+else:
+    print("'%s' is not a palindrome" %(word))
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
