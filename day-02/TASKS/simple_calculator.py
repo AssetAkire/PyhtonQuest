19e16f2b5cb69ea7debe0aff9ae5fa288f501061
Tessa Erika <116341897+AssetAkire@users.noreply.github.com>
2024-02-19 14:31:04 +0800

    simple_calculator.py

--- day-02/TASKS/simple_calculator.py
+++ day-02/TASKS/simple_calculator.py
@@ -1,4 +1,8 @@
 # Write the code ↓ to read user's input for two operands and selected operation.
+print("SIMPLE CALCULATOR FOR ALF\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, x, /): ")
 # NOTE: The two operands must be read as floats.
 
 
@@ -11,6 +15,17 @@
 
 # Write the code ↓ to perform the calculations based on the selected operation.
 
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "x":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    print("Invalid operator. Please choose +, -, x, or /.")
+    exit()
 
 
 
@@ -19,6 +34,7 @@
 
  
 # Write the code ↓ to display the result.
+print(f"The result of {num1:.1f} {operator} {num2:.1f} is {result:.1f}")
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
