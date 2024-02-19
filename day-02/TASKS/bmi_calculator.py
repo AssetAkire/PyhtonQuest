f620b79b7922f1c40b5759a75043c7ca0f387026
Tessa Erika <116341897+AssetAkire@users.noreply.github.com>
2024-02-20 01:53:13 +0800

    bmi_calculator.py

--- day-02/TASKS/bmi_calculator.py
+++ day-02/TASKS/bmi_calculator.py
@@ -9,6 +9,11 @@ ABOVE 30.0          OBESITY
 """
  
 # Write the code ↓ to read user's height and weight.
+print("Simple Calculator for Alf")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, x, /): ")
+result = 0.0
 # Be cautious when reading input of various data types.
 
 
@@ -21,6 +26,16 @@ ABOVE 30.0          OBESITY
 
 # Write the code ↓ to perform the calculations of user's BMI and categorize its status.
 
+if operator == '+':
+    answer = num1 + num2
+elif operator == '-':
+    answer = num1 - num2
+elif operator == 'x':
+    answer = num1 * num2
+elif operator == '/':
+    answer = num1 / num2
+else:
+    print("Invalid input")
 
 
 
@@ -28,6 +43,8 @@ ABOVE 30.0          OBESITY
 
 
 # Write the code ↓ to display the user's BMI and its classification.
+print("The result of %.2f %s %.2f is %.2f" %(num1, operator, num2, result))
+
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 # Use :.2f format specifier when printing the BMI value to display the BMI with only two decimal places.
 
