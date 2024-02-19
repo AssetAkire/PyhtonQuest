8e5206a5a8e1302bc44864bdaf0ee5daf6f9362b
Tessa Erika <116341897+AssetAkire@users.noreply.github.com>
2024-02-20 02:11:27 +0800

    cylinder-volume_calculator.py

--- day-04/TASKS/cylinder-volume_calculator.py
+++ day-04/TASKS/cylinder-volume_calculator.py
@@ -1,6 +1,7 @@
 import math
 
 # Write the code ↓ to read the radius and height of a cylinder from the user.
+print("CYLINDER VOLUME CALCULATOR FOR ALF")
 # Be cautious when reading input of various data types.
 
 
@@ -9,6 +10,15 @@ import math
 
 
 # Write the code ↓ to calculate the volume of the cylinder using the formula V = πr^2h.
+radius = float(input("Please, enter the radius of the cylinder: "))
+while radius < 0:
+    print("Invalid input")
+    radius = float(input("Please, enter the radius of the cylinder: "))
+    
+height = float(input("Please, enter the radius of the cylinder: "))
+while height < 0:
+    print("Invalid input")
+    height = float(input("Please, enter the radius of the cylinder: "))
 # Formula to calculate the volume (V) of a cylinder:
 # V = π * r^2 * h
 
@@ -17,6 +27,9 @@ import math
 
 
 # Write the code ↓ to display the calculated volume with 2 decimal places.
+volume = math.pi * (radius * radius) * height
+
+print("The volume of the cylinder is: %.2f" %(volume))
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
