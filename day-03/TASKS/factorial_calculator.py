ef865a1add63f875af39d14b8ae4bdd265d3694d
Tessa Erika <116341897+AssetAkire@users.noreply.github.com>
2024-02-20 01:58:50 +0800

    factorial_calculator.py

--- day-03/TASKS/factorial_calculator.py
+++ day-03/TASKS/factorial_calculator.py
@@ -1,4 +1,6 @@
 # Write the code ↓ to read the user's input for a non-negative integer.
+print("FACTORIAL CALCULATOR FOR ALF")
+number = int(input("\nPlease, enter a non-negative integer: "))
 # Be cautious when reading input of various data types.
 
 
@@ -7,13 +9,21 @@
 
 
 # Write the code ↓ to calculate the factorial of the user-defined integer using a loop.
+while number < 0:
+    print("Invalid input")
+    number = int(input("Please, enter a non-negative integer: "))
+    
+result = 1
 
+for i in range(1, number + 1):
+    result *= i
 
 
 
 
 
 # Write the code ↓ to display the factorial result.
+print("The factorial of %d is: %d" %(number, result))
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
