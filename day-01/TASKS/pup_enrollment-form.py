62896e854c5f46db9f1205b40f1ef7446d90bd91
Tessa Erika <116341897+AssetAkire@users.noreply.github.com>
2024-02-19 14:10:39 +0800

    pup_enrollment-form.py

--- day-01/TASKS/pup_enrollment-form.py
+++ day-01/TASKS/pup_enrollment-form.py
@@ -1,4 +1,9 @@
 # Write the code ↓ to read user's input.
+print("PUP Enrollment Form")
+user_name = input("Enter your full name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your previous average: "))
+is_cloudArchitect = input("Are you a member of the AWS Cloud Club - Department of Cloud Computing? (yes/no): ").lower() == "yes"
 # Be cautious when reading input of various data types.
 
 
@@ -7,6 +12,14 @@
 
 
 # Write the code ↓ to display the user's personal information.
+print("\nPersonal Information:")
+print(f"Name: {user_name}")
+print(f"Age: {age} years")
+print(f"Previous average: {height:.2f}")
+if is_cloudArchitect:
+    print("AWS Cloud Club Member: Yes")
+else:
+    print("AWS Cloud Club Member: No")
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
 
